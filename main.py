0ac3009b00b4ecbd17680a0b5914c4ad9c86f1b4
Joselito Guardado <jaguardado123@gmail.com>
2019-10-16 18:47:36 -0500

    Second

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 import requests
+from ebaysdk.finding import Connection as finding
+from bs4 import BeautifulSoup
 
-response = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
+Keywords = input('Enter your Keywords: \n')
+api = finding(appid='JamesCan-HiMilesp-PRD-c246ab013-815fa751',config_file=None)
+api_request = { 'keywords':Keywords, 'outputSelector': 'SellerInfo'}
 
-code = response.status_code
-print(code)
+response = api.execute('findItemsByKeywords', api_request)
+soup = BeautifulSoup(response.content, 'lsml')
 
-if code == 200:
-    print(response.json())
+totalentries = int(soup.find('totalentries').text)
+items = soup.find_all('item')
+
+#input(items[0])
+
+for item in items:
+    cat = item.categoryname.string.lower()
+    title = item.title.string.lower().strip()
+    price = int(round(float(item.currentprice.string)))
+    url = item.viewitemurl.string.lower()
+    seller = item.sellerusername.test.lower()
+    listingtype = item.listingtype.string.lower()
+    condition = item.conditiondisplayname.string.lower()
+
+    print('________________')
+    print(cat)
+    print()
+    print(title)
+    print()
+    print(price)
+    print()
+    print(url)
+    print()
+    print(seller)
+    print()
+    print(listingtype)
+    print()
+    print(condition)
+    print()
+
+#response = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
+
+#code = response.status_code
+#print(code)
+
+#if code == 200:
+#    print(response.json())
