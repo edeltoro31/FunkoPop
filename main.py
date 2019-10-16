28b4777d6ade4dd9c83d1d21d0963a8e00630bc3
Joselito Guardado <jaguardado123@gmail.com>
2019-10-16 18:57:35 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
-import requests
 from ebaysdk.finding import Connection as finding
 from bs4 import BeautifulSoup
 
 Keywords = input('Enter your Keywords: \n')
-api = finding(appid='JamesCan-HiMilesp-PRD-c246ab013-815fa751',config_file=None)
-api_request = { 'keywords':Keywords, 'outputSelector': 'SellerInfo'}
+# api = finding(appid='JamesCan-HiMilesp-PRD-c246ab013-815fa751',config_file=None)
+# api_request = { 'keywords': Keywords, 'outputSelector': 'SellerInfo'}
 
-response = api.execute('findItemsByKeywords', api_request)
-soup = BeautifulSoup(response.content, 'lsml')
+# response = api.execute('findItemsByKeywords', api_request)
+# soup = BeautifulSoup(response.content, 'lxml')
 
-totalentries = int(soup.find('totalentries').text)
-items = soup.find_all('item')
+# totalentries = int(soup.find('totalentries').text)
+# items = soup.find_all('item')
 
-#input(items[0])
+# input(items[0])
 
-for item in items:
-    cat = item.categoryname.string.lower()
-    title = item.title.string.lower().strip()
-    price = int(round(float(item.currentprice.string)))
-    url = item.viewitemurl.string.lower()
-    seller = item.sellerusername.test.lower()
-    listingtype = item.listingtype.string.lower()
-    condition = item.conditiondisplayname.string.lower()
+# for item in items:
+#     cat = item.categoryname.string.lower()
+#     title = item.title.string.lower().strip()
+#     price = int(round(float(item.currentprice.string)))
+#     url = item.viewitemurl.string.lower()
+#     seller = item.sellerusername.test.lower()
+#     listingtype = item.listingtype.string.lower()
+#     condition = item.conditiondisplayname.string.lower()
 
-    print('________________')
-    print(cat)
-    print()
-    print(title)
-    print()
-    print(price)
-    print()
-    print(url)
-    print()
-    print(seller)
-    print()
-    print(listingtype)
-    print()
-    print(condition)
-    print()
+#     print('________________')
+#     print(cat)
+#     print()
+#     print(title)
+#     print()
+#     print(price)
+#     print()
+#     print(url)
+#     print()
+#     print(seller)
+#     print()
+#     print(listingtype)
+#     print()
+#     print(condition)
+#     print()
 
 #response = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
 
